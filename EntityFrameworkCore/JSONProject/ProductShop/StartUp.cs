f163d402a5f349f86e2f96d720b021293faa018f
MiBuena <athina.palada@gmail.com>
2019-11-27 13:58:57 +0200

    Adding json tasks

---
+++ EntityFrameworkCore/JSONProject/ProductShop/StartUp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new ProductShopContext())
+            {
+                var inputJson = File.ReadAllText("./../../../Resources/ProductShop/categories.json");
+
+                //var result = ImportCategories(context, inputJson);
+
+                //Console.WriteLine(result);
+            }
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var categories = JsonConvert.DeserializeObject<List<Category>>(inputJson)
+                .Where(x => x.Name != null)
+                .ToList();
+
+            context.Categories.AddRange(categories);
+
+            var insertedRecords = context.SaveChanges();
+
+            var result = $"Successfully imported {insertedRecords}";
+
+            return result;
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+
+            context.Products.AddRange(products);
+
+            var insertedRecords = context.SaveChanges();
+
+            var result = $"Successfully imported {insertedRecords}";
+
+            return result;
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            context.AddRange(users);
+
+            var insertedRecords = context.SaveChanges();
+
+            var result = $"Successfully imported {insertedRecords}";
+
+            return result;
+        }
+    }
+}
