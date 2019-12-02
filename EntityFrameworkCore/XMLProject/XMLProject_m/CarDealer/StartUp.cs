3134e4ea2e2541ffd446979acd940b3e13961d23
MiBuena <athina.palada@gmail.com>
2019-12-02 20:13:30 +0200

    Adding CarDealer

---
+++ EntityFrameworkCore/XMLProject/XMLProject_m/CarDealer/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using CarDealer.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new CarDealerContext())
+            {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
