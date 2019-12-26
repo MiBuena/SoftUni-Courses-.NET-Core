4e6ca645f9e71f0c6374cbd7991fd7913bc048ab
MiBuena <athina.palada@gmail.com>
2019-12-26 17:13:56 +0200

    Adding application

---
+++ WebAdvanced/WebApplicationWithIdentity/WebApplicationWithIdentity/Services/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationWithIdentity.Services
+{
+    public class DataService : IDataService
+    {
+        public int GetCount()
+        {
+            return 1;
+        }
+    }
+}
---
+++ WebAdvanced/WebApplicationWithIdentity/WebApplicationWithIdentity/TagHelpers/HelloTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationWithIdentity.TagHelpers
+{
+    [HtmlTargetElement("h1", Attributes = "target-name")]
+    public class HelloTagHelper : TagHelper
+    {
+        private const string MessageFormat = "Hello, {0}";
+        public string TargetName { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string formattedMessage = string.Format(MessageFormat, this.TargetName);
+            output.Content.SetContent(formattedMessage);
+        }
+    }
+}
