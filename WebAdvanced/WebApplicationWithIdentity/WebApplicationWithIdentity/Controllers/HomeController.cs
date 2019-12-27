e80e8515cd68441a2bb58befa57b06abb9d83856
MiBuena <athina.palada@gmail.com>
2019-12-27 14:58:04 +0200

    Adding appsettings

--- WebAdvanced/WebApplicationWithIdentity/WebApplicationWithIdentity/Controllers/HomeController.cs
+++ WebAdvanced/WebApplicationWithIdentity/WebApplicationWithIdentity/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using WebApplicationWithIdentity.Models;
 
@@ -12,10 +13,12 @@ namespace WebApplicationWithIdentity.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
