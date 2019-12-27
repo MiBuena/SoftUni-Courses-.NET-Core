b890bdaab6afe60bf6134d749a9c1a6d5ee6eba9
MiBuena <athina.palada@gmail.com>
2019-12-27 21:26:40 +0200

    Adding view component

---
+++ WebAdvanced/WebApplicationWithIdentity/WebApplicationWithIdentity/ViewComponents/HelloWorldViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationWithIdentity.Services;
+
+namespace WebApplicationWithIdentity.ViewComponents
+{
+    public class HelloWorldViewComponent : ViewComponent
+    {
+
+        private readonly IDataService dataService;
+
+        public HelloWorldViewComponent(IDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var a = new ComponentModel()
+            {
+                Name = "aa"
+            };
+
+            return View(a);
+        }
+    }
+
+    public class ComponentModel
+    {
+        public string Name { get; set; }
+    }
+}
+
+
